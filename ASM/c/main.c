4fa0b2af2462cd8fd9ec2f3f1703f2015b2b3e6e
krimtonz <krimtonz@gmail.com>
2018-11-03 19:35:41 -0500

    age-swap

--- ASM/c/main.c
+++ ASM/c/main.c
@@ -18,7 +18,19 @@ void c_init() {
     text_init();
 }
 
-static int button_time[16];
+void zu_execute_game(int16_t entrance_index, uint16_t cutscene_index)
+{
+	z64_file.entrance_index = entrance_index;
+	z64_file.cutscene_index = cutscene_index;
+	z64_file.interface_flag = 0;
+	if (z64_file.minigame_state == 1)
+		z64_file.minigame_state = 3;
+	z64_game.entrance_index = entrance_index;
+	z64_ctxt.state_continue = 0;
+	z64_ctxt.next_ctor = z64_state_ovl_tab[3].vram_ctor;
+	z64_ctxt.next_size = z64_state_ovl_tab[3].ctxt_size;
+}
+
 void c_after_game_state_update() {
     if (z64_game.pause_state == 6 &&
                 z64_game.pause_screen == 0 &&
@@ -38,6 +50,23 @@ void c_after_game_state_update() {
 			pad_pressed |= p;
 	}
 
+	if (pad_pressed & 0x0800) {
+		int age = z64_file.link_age;
+		z64_file.link_age = z64_game.link_age;
+		z64_game.link_age = !z64_game.link_age;
+		z64_SwitchAgeEquips();
+		z64_file.link_age = age;
+		for (int i = 0; i < 4; ++i)
+			if (z64_file.button_items[i] != Z64_ITEM_NULL)
+				z64_UpdateItemButton(&z64_game, i);
+		z64_UpdateEquipment(&z64_game, &z64_link);
+
+
+
+		zu_execute_game(z64_file.entrance_index, 0x0000);
+
+	}
+
 	if (z64_file.link_age==0) {
 
 		if (pad_pressed & 0x0200 && z64_file.iron_boots) {
@@ -51,5 +80,7 @@ void c_after_game_state_update() {
 			else z64_file.equip_boots = 3;
 			z64_UpdateEquipment(&z64_game, &z64_link);
 		}
+
+		
 	}
 }
