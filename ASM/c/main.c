4c499d4705f847611088a2a55cd32246ca55e7f3
krimtonz <krimtonz@gmail.com>
2018-11-07 11:39:07 -0600

    Implemented lazy crash prevention for spawn points that don't exist at a current age

--- ASM/c/main.c
+++ ASM/c/main.c
@@ -12,9 +12,26 @@
     0x10000000 | \
     0x20000000)     
 
+typedef struct {
+	uint8_t age;
+	uint16_t search;
+	uint16_t repl;
+}age_swap_table_t;
+
 static uint16_t pad_pressed_raw,
 pad,
 pad_pressed;
+
+static age_swap_table_t age_swap_table[8] = {
+	{1,0x05E8,0x0209},
+	{1,0x05E9,0x0209},
+	{1,0x05EA,0x0209},
+	{1,0x05EB,0x0209},
+	{1,0x0580,0x0205},
+	{1,0x0581,0x0205},
+	{1,0x0582,0x0205},
+	{1,0x0583,0x0205}
+};
 
 void c_init() {
 	pad_pressed_raw = 0;
@@ -24,7 +41,6 @@ void c_init() {
 	gfx_init();
 	text_init();
 }
-
 
 void c_after_game_state_update() {
 	if (z64_game.pause_state == 6 &&
@@ -45,7 +61,18 @@ void c_after_game_state_update() {
 
 		if (pad_pressed & 0x0800) {
 			z64_game.link_age = !z64_game.link_age;
-			z64_game.entrance_index = z64_file.entrance_index;
+
+			uint16_t entrance = z64_file.entrance_index;
+			for (int i = 0; i < sizeof(age_swap_table) / sizeof(age_swap_table_t); i++) {
+				if (z64_game.link_age == age_swap_table[i].age && age_swap_table[i].search == entrance) {
+					entrance = age_swap_table[i].repl;
+					break;
+				}
+			}
+
+			z64_game.entrance_index = entrance;
+			
+			
 			z64_game.scene_load_flag = 0x14;
 
 		}
